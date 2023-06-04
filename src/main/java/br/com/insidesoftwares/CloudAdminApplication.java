bf30434f49ae82b252a34d3697cec2b8a64b335c
Samuel Cunha <samukasaww@live.com>
2023-06-03 22:35:06 -0300

    Atualização para Spring Boot 3.1.0 e Realizando ajustes para o TCC

--- src/main/java/br/com/insidesoftwares/CloudAdminApplication.java
+++ src/main/java/br/com/insidesoftwares/CloudAdminApplication.java
@@ -3,9 +3,11 @@ package br.com.insidesoftwares;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableEurekaServer
 public class CloudAdminApplication {
 
     public static void main(String[] args) {
