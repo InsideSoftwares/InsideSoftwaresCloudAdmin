48034bcfe21d074b6f1ab9e165b38192fd0b0eff
Samuel Cunha <samukasaww@live.com>
2023-11-20 08:55:58 -0300

    Atualização de Versão e removendo Spring admin

--- src/main/java/br/com/insidesoftwares/CloudAdminApplication.java
+++ src/main/java/br/com/insidesoftwares/CloudAdminApplication.java
@@ -1,12 +1,10 @@
 package br.com.insidesoftwares;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableAdminServer
 @EnableEurekaServer
 public class CloudAdminApplication {
 
